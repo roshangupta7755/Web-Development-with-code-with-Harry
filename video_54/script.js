ee38b443d1f97a5903ba8560191440e58acaab2e
Roshan Gupta <roshangupta112113@gmail.com>
2025-11-07 21:28:57 +0530

    Add files via upload

---
+++ video_54/script.js
@@ -0,0 +1,24 @@
+alert("Hello User");
+
+
+console.log("Code is runing..");
+console.log("Code is also runing..");
+console.log("Code is  wo runing..");
+
+var a = prompt("Enter your number")
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer ")
+
+if(isTrue){
+    console.log("Computer is blasting")
+}
+
+else{
+    console.log("Computer is not blasting")
+
+}
+
+console.log("Your number is " +a);
+
+document.title = "Hey , I am Roshan"
+
+// document.body.style.backgroundColor = "red"
