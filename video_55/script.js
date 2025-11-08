c1dacd17f773940d31f599b1a2f97e207b1dc1e7
Roshan Gupta <roshangupta112113@gmail.com>
2025-11-08 18:58:00 +0530

    Add files via upload

---
+++ video_55/script.js
@@ -0,0 +1,30 @@
+console.log( "Hello , user")
+
+var a = 5 , b = 6;
+var c = "Roshan";
+
+console.log(a + b)
+
+console.log(typeof a, typeof b, typeof c )
+
+let x = "Roshan bhai";
+let y = 242;
+let z = 33.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    "name": "Harry",
+    "job code": 5600,
+    "is_handsome": true
+}
+
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o);
