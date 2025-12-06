4c8e6cadf35a94206138952d124f7b10762d8373
Roshan Gupta <roshangupta112113@gmail.com>
2025-12-07 00:34:11 +0530

    Add files via upload

---
+++ video_67/script.js
@@ -0,0 +1,13 @@
+console.log("Hello Roshan.....")
+
+document.body.firstElementChild 
+document.body.firstElementChild.childNodes 
+document.body.firstElementChild.children    
+
+
+
+ document.body.firstElementChild.children[3]
+ document.body.firstElementChild.children[3].nextElementSibling
+ document.body.firstElementChild.children[3].previousElementSibling
+ document.body.firstElementChild.children[3].previousSibling
+ 
