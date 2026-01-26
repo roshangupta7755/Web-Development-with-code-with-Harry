bdea1e14befe6dce0dd6f4a236b38aef1d98442a
Roshan Gupta <roshangupta112113@gmail.com>
2026-01-26 14:54:33 +0530

    Add files via upload

---
+++ video_87/aboutpath.js
@@ -0,0 +1,10 @@
+import path from "path"
+
+let mypath = "C:\\Users\\rosha\\Music\\web_hrray\\video_87\\ROshan.txt"
+
+console.log(path.extname(mypath));
+console.log(path.dirname(mypath));
+console.log(path.basename(mypath));
+
+console.log(path.join ("c:/" , "progrms\\roshan.txt") );
+
