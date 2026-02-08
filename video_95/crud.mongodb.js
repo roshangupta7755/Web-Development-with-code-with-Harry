f0b706f74baefd12e88d05e50acb0a241ff3d66d
Roshan Gupta <roshangupta112113@gmail.com>
2026-02-08 13:13:14 +0530

    Uncomment data insertion and deletion operations

--- video_95/crud.mongodb.js
+++ video_95/crud.mongodb.js
@@ -19,70 +19,68 @@ db.createCollection("courses")
 //     projects: 45
 // })
 
-// db.courses.insertMany([
-//     {
-//       "name": "Python Masterclass",
-//       "price": 0,
-//       "assignments": 10,
-//       "projects": 30
-//     },
-//     {
-//       "name": "JavaScript Basics",
-//       "price": 0,
-//       "assignments": 8,
-//       "projects": 20
-//     },
-//     {
-//       "name": "C# for Beginners",
-//       "price": 0,
-//       "assignments": 15,
-//       "projects": 40
-//     },
-//     {
-//       "name": "Web Development Fundamentals",
-//       "price": 0,
-//       "assignments": 12,
-//       "projects": 35
-//     },
-//     {
-//       "name": "Java Programming Essentials",
-//       "price": 0,
-//       "assignments": 14,
-//       "projects": 38
-//     },
-//     {
-//       "name": "ReactJS Crash Course",
-//       "price": 0,
-//       "assignments": 10,
-//       "projects": 25
-//     },
-//     {
-//       "name": "SQL Simplified",
-//       "price": 0,
-//       "assignments": 12,
-//       "projects": 30
-//     },
-//     {
-//       "name": "Responsive Web Design",
-//       "price": 0,
-//       "assignments": 10,
-//       "projects": 28
-//     },
-//     {
-//       "name": "Node.js for Beginners",
-//       "price": 0,
-//       "assignments": 13,
-//       "projects": 36
-//     },
-//     {
-//       "name": "Frontend Development with Vue.js",
-//       "price": 0,
-//       "assignments": 11,
-//       "projects": 32
-//     }
-//   ]
-//   )
-
+db.courses.insertMany([
+  {
+    name: "Python Masterclass",
+    price: 0,
+    assignments: 10,
+    projects: 30
+  },
+  {
+    name: "JavaScript Basics",
+    price: 0,
+    assignments: 8,
+    projects: 20
+  },
+  {
+    name: "C# for Beginners",
+    price: 0,
+    assignments: 15,
+    projects: 40
+  },
+  {
+    name: "Web Development Fundamentals",
+    price: 0,
+    assignments: 12,
+    projects: 35
+  },
+  {
+    name: "Java Programming Essentials",
+    price: 0,
+    assignments: 14,
+    projects: 38
+  },
+  {
+    name: "ReactJS Crash Course",
+    price: 0,
+    assignments: 10,
+    projects: 25
+  },
+  {
+    name: "SQL Simplified",
+    price: 0,
+    assignments: 12,
+    projects: 30
+  },
+  {
+    name: "Responsive Web Design",
+    price: 0,
+    assignments: 10,
+    projects: 28
+  },
+  {
+    name: "Node.js for Beginners",
+    price: 0,
+    assignments: 13,
+    projects: 36
+  },
+  {
+    name: "Frontend Development with Vue.js",
+    price: 0,
+    assignments: 11,
+    projects: 32
+  }
+])
 // READ
 // let a = db.courses.find({price: 0}) 
 
@@ -107,5 +105,6 @@ db.courses.deleteMany({price: 1000})
 
 
 // https://www.mongodb.com/docs/manual/reference/operator/query/
+
 
 
